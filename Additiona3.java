8d3e443e537f6deb722e9e3f8655625a63d9fc52
satishkumarpasala <151707444+satishkumarpasala@users.noreply.github.com>
2024-09-08 11:12:25 +0300

    Create Additiona3.java

---
+++ Additiona3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Addition3 extends Object 
+{					    
+	public static void main(String args[])
+	{
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two integers for Addition : ");
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=x+y;
+		System.out.println("Sum of "+x+" and "+y+" is "+z);
+	}
+}
