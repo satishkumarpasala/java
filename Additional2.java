ed41a967523fb1ccef6e63e6a0a92cd7aedd86f0
satishkumarpasala <151707444+satishkumarpasala@users.noreply.github.com>
2024-09-08 11:14:02 +0300

    Create Additional2.java

---
+++ Additional2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Addition2   
+{					    
+	public static void main(String args[])
+	{
+		int x=Integer.parseInt(args[0]);
+		int y=Integer.parseInt(args[1]);
+		int z=x+y;
+		System.out.println("Sum of "+x+" and "+y+" is "+z);
+	}
+}
